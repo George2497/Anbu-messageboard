3701f3c87f0b1a401964eae03eaa13ab9933e0c7
George Koundouri <georgekoundouri@msn.com>
2024-11-12 20:35:02 +0000

    Admin's can now edit and delete newly created posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,4 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
-  def requireLogin
-    if !logged_in?
-      flash[:alert] = "You need to be logged in to logged in to access this area!"
-      redirect_to login_path
-    end
-  end
 end
