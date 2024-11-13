3dbbd1fb1c9dcf61a005af3db45c29db4c4a5523
George Koundouri <georgekoundouri@msn.com>
2024-11-13 20:06:38 +0000

    Added shutup gem to stop server not being able to run due to another server currently running and added restrictions so users that are not logged in cannot access the application and get redirected to the login page and they are then greeted with an alert message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
-  helper_method :logged_in?, :current_user
-
-  def logged_in?
-    !!current_user
-  end
+  helper_method :current_user, :logged_in?
 
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
+  def logged_in?
+    !!current_user
+  end
+
+  def requireLogin
+    if !logged_in?
+      flash[:alert] = "You need to be logged in to access this area!"
+      redirect_to login_path
+    end
+  end
 end
