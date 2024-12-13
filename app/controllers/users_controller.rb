bf104a9c92277289b730dacd0f22418fb5aa4d07
George Koundouri <georgekoundouri@msn.com>
2024-12-13 17:00:38 +0000

    Fixed a bug which was stopping user to be able to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :requireLogin, except: [:new]
+  before_action :requireLogin, except: [:new, :create]
   before_action :set_user, only: [:show, :edit, :update]
 
   def index
